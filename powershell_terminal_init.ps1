a417398dce6bb8553961d8835b1cb1a638094cbd
Konrad Polarczyk <konrad.polarczyk@medius.com>
2024-03-04 09:46:52 +0100

    Change font

--- powershell_terminal_init.ps1
+++ powershell_terminal_init.ps1
@@ -4,3 +4,14 @@ oh-my-posh font install Meslo
 
 # Run this from VSCode profile aswell
 Copy-Item $PSScriptRoot\profile.ps1 $profile
+
+
+# Change font in vs code integrated terminal
+$vsCodeSettingsPath = "$($env:AppData)\Code\User\settings.json"
+
+$vsCodeSettings = Get-Content $vsCodeSettingsPath | ConvertFrom-Json
+$vsCodeSettings.'terminal.integrated.fontFamily' = "MesloLGL Nerd Font Mono"
+
+$vsCodeSettings | ConvertTo-Json | Out-File $vsCodeSettingsPath
+
+# Change font in other editors that use powershell
