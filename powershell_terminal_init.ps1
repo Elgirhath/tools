2e3f08f9eb0930427dc2bccd12c5359f414ead93
User <konradpolarczyk98@gmail.com>
2024-07-09 10:26:00 +0200

    More automation

--- powershell_terminal_init.ps1
+++ powershell_terminal_init.ps1
@@ -1,6 +1,10 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
-# oh-my-posh font install Meslo
+oh-my-posh font install Meslo
+
+if (!(Test-Path $profile)) {
+    New-Item $profile -Force
+}
 
 $profileDir = Split-Path $profile -Parent
 
@@ -8,14 +12,31 @@ $profileDir = Split-Path $profile -Parent
 Copy-Item $PSScriptRoot\profile.ps1 $profile
 Copy-Item $PSScriptRoot\check.psm1 $profileDir
 
+if ($env:TERM_PROGRAM -eq "vscode") {
+    # Change font in vs code integrated terminal
+    $vsCodeSettingsPath = "$($env:AppData)\Code\User\settings.json"
+    
+    $vsCodeSettings = Get-Content $vsCodeSettingsPath | ConvertFrom-Json
+    $vsCodeSettings = [pscustomobject] @{ 
+        "terminal" = [pscustomobject] @{
+            "integrated" = [pscustomobject] @{
+                "fontFamily" = "MesloLGL Nerd Font Mono"
+            }
+        }
+    }
+    
+    $vsCodeSettings | ConvertTo-Json -Depth 20 | Out-File $vsCodeSettingsPath -Encoding utf8
+}
 
-# Change font in vs code integrated terminal
-$vsCodeSettingsPath = "$($env:AppData)\Code\User\settings.json"
+$windowsTerminalSettingsPath = Get-ChildItem "$env:LOCALAPPDATA/Packages/Microsoft.WindowsTerminal*/LocalState/settings.json"
+$windowsTerminalSettings = Get-Content $windowsTerminalSettingsPath | ConvertFrom-Json
+$windowsTerminalSettings.profiles.defaults = [pscustomobject] @{
+    "font" = [pscustomobject] @{
+        "face" = "MesloLGL Nerd Font Mono"
+    }
+}
 
-$vsCodeSettings = Get-Content $vsCodeSettingsPath | ConvertFrom-Json
-$vsCodeSettings.'terminal.integrated.fontFamily' = "MesloLGL Nerd Font Mono"
-
-$vsCodeSettings | ConvertTo-Json | Out-File $vsCodeSettingsPath
+$windowsTerminalSettings | ConvertTo-Json -Depth 20 | Out-File $windowsTerminalSettingsPath
 
 # Change font in other editors that use powershell
 
